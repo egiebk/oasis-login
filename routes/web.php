a9afd9b4052ea42ab98e6112735cc97f7d79e6ce
Reggie M. Pistanio <r.pistanio@outlook.com>
2025-02-21 17:15:45 +0800

    Revert "Merge branch 'main' of https://github.com/egiebk/oasis"
    This reverts commit 967df1d7dbb2cf86afdd16880dc35cec65f509bb, reversing
    changes made to 692268047ca3be20ab0883fd9667cf5881d5f7d7.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
 Route::get('/', function () {
     return view('welcome');
 });
